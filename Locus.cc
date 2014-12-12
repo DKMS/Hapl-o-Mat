3e1b1d16debada47caeb16076a2042e8ad2c732c
Christian Schäfer <KrissSchaefer@users.noreply.github.com>
2014-12-12 13:38:06 +0100

    reset

--- Locus.cc
+++
@@ -1,9 +0,0 @@
-#include "Locus.h"
-
-GLLocus::GLLocus(const std::string code){
-
-}
-
-HLocus::HLocus(const std::string code){
-
-}
--- include/Typedefs.h
+++
@@ -1,9 +0,0 @@
-#ifndef Typedefs_header
-#define Typedefs_header
-
-#include<vector>
-#include<string>
-
-typedef std::vector<std::string> strVec_t;
-
-#endif
