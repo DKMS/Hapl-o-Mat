0a36d0c873e567bbf09c5594600507e408e19e60
Christian Schäfer <KrissSchaefer@users.noreply.github.com>
2014-12-17 09:34:23 +0100

    Fixed insertion at loci. Gave a segfault

--- Locus.cc
+++ Locus.cc
@@ -11,7 +11,7 @@ void PhasedLocus::resolve(){
   for(auto locusPosition : phasedLocus){
     double alleleFrequency = 1. / static_cast<double>(phasedLocus.size());
     for(auto code : locusPosition){
-      std::shared_ptr<Allele> pAllele = createAllele(code, wantedPrecision, alleleFrequency);
+      std::shared_ptr<Allele> pAllele = Allele::createAllele(code, wantedPrecision, alleleFrequency);
       std::vector<std::shared_ptr<Allele>> listOfpAlleles = pAllele->translate();
     }//for LocusPosition
   }//for phasedLocus
@@ -20,24 +20,50 @@ void PhasedLocus::resolve(){
 void UnphasedLocus::resolve(){
 
   std::cout << "unphased" << std::endl;
-  auto locusPositionResult = pAllelesAtBothLocusPositions.begin();
+  //H2filter if [0],[1] have more than one element
+
   for(auto locusPosition : unphasedLocus){
+    std::vector<std::shared_ptr<Allele>> allPAllelesAtOneLocusPosition;
     for(auto code : locusPosition){
       double alleleFrequency = 1. / static_cast<double>(locusPosition.size());
-      std::shared_ptr<Allele> pAllele = createAllele(code, wantedPrecision, alleleFrequency);
+      std::shared_ptr<Allele> pAllele = Allele::createAllele(code, wantedPrecision, alleleFrequency);
       std::vector<std::shared_ptr<Allele>> pAllelesAtOneLocusPosition = pAllele->translate();
-      locusPositionResult->insert(locusPositionResult->end(), pAllelesAtOneLocusPosition.begin(), pAllelesAtOneLocusPosition.end());
+      allPAllelesAtOneLocusPosition.insert(allPAllelesAtOneLocusPosition.end(), pAllelesAtOneLocusPosition.begin(), pAllelesAtOneLocusPosition.end());
     }
-    locusPositionResult ++;
+    pAllelesAtBothLocusPositions.push_back(allPAllelesAtOneLocusPosition);
   }
 
   for(auto it : pAllelesAtBothLocusPositions){
     for(auto it2 : it){
+      std::cout << "summary" << std::endl;
       std::cout << it2->getCode() << std::endl;
       std::cout << it2->getFrequency() << std::endl;
       it2->printCodePrecision(it2->getPrecision());
     }
     std::cout << std::endl;
   }
+
+}
+
+void UnphasedLocus::buildResolvedPhasedLocus(){ 
+
+  std::vector<std::vector<int>> in;
+  std::vector<std::vector<int>> out;
+  std::vector<int> a;
+  a.push_back(1);
+  a.push_back(2);
+  a.push_back(3);
+  std::vector<int> b;
+  b.push_back(4);
+  b.push_back(5);
+  b.push_back(6);  
+
+  in.push_back(a);
+  in.push_back(b);
+
+  //  cartesianProduct(out, in);
+
+  //  cartesianProduct(pAllelesAtPhasedLocus, pAllelesAtBothLocusPositions);
+                     
 
 }
--- include/Locus.h
+++ include/Locus.h
@@ -10,7 +10,7 @@ class Locus{
 
  public:
 
-  explicit Locus() : resolvedPhasedLocus(), wantedPrecision(){}
+  explicit Locus() : pAllelesAtPhasedLocus(), wantedPrecision(){}
   virtual ~Locus(){}
   
   virtual void resolve() = 0;
@@ -19,7 +19,7 @@ class Locus{
   void setWantedPrecision(const Allele::codePrecision in_wantedPrecision) {wantedPrecision = in_wantedPrecision;}
 
  protected:
-  std::vector<std::array<std::shared_ptr<Allele>, 2>> resolvedPhasedLocus;
+  std::vector<std::array<std::shared_ptr<Allele>, 2>> pAllelesAtPhasedLocus;
   Allele::codePrecision wantedPrecision;
 };
 
@@ -44,7 +44,10 @@ class PhasedLocus : public Locus{
 class UnphasedLocus : public Locus{
 
  public:
-  explicit UnphasedLocus(const strVecArr_t & in_unphasedLocus) : Locus(), unphasedLocus(in_unphasedLocus), pAllelesAtBothLocusPositions(){}
+  explicit UnphasedLocus(const strVecArr_t & in_unphasedLocus) : Locus(), unphasedLocus(in_unphasedLocus), pAllelesAtBothLocusPositions()
+    {
+      pAllelesAtBothLocusPositions.reserve(2);
+    }
   explicit UnphasedLocus(const strVecArr_t & in_unphasedLocus,
 			 const Allele::codePrecision in_wantedPrecision)
     : Locus(),
@@ -52,15 +55,17 @@ class UnphasedLocus : public Locus{
     pAllelesAtBothLocusPositions()
     {
       setWantedPrecision(in_wantedPrecision);
+      pAllelesAtBothLocusPositions.reserve(2);
     }
 
   virtual void resolve();
 
   void H2Filter();
+  void buildResolvedPhasedLocus();
 
  private:
   strVecArr_t unphasedLocus;
-  std::array<std::vector<std::shared_ptr<Allele>>,2> pAllelesAtBothLocusPositions;
+  std::vector<std::vector<std::shared_ptr<Allele>>> pAllelesAtBothLocusPositions;
 };
 
 #endif
