cbb3982343b00bbb4c721b8d4e503ca3ef0e9833
Christian Schäfer <KrissSchaefer@users.noreply.github.com>
2015-01-20 15:09:55 +0100

    script to build g from P and G codes

---
+++ Python/buildH1gFromP/BuildH1g.py
@@ -0,0 +1,52 @@
+
+
+#save each line of P.txt as P-code plus a set of the corresponding codes
+allPCodes = []
+with open('P.txt') as file:
+    for line in file:
+        line = line.rstrip('\n')
+        splittedLine = line.split()
+        pCode = []
+        pCode.append(splittedLine[0])
+
+        splittedLine.pop(0)
+        codes = set()
+        codes.update(splittedLine)
+        pCode.append(codes)
+        
+        allPCodes.append(pCode)
+
+#
+endLetters = ('N', 'L', 'S', 'Q')
+with open('H1.txt') as file:
+    for line in file:
+        line = line.rstrip('\n')
+        splittedLine = line.split()
+        #build two sets, one containing codes with letters at the end, the other one the remaining codes
+        splittedLine.pop(0)
+        codesN = set()
+        codes = set()
+        for code in splittedLine:
+            if code.endswith(endLetters):
+                codesN.add(code)
+            else:
+                codes.add(code)
+
+        print codes
+        for pCode in allPCodes:
+            if codes.issubset(pCode[1]) and codes:
+                pCode[1].update(codesN)
+                print pCode[0]
+        
+with open('H1g.txt', 'w') as file:
+    for pCode in allPCodes:
+        gCode = pCode[0][:-1] + 'g'
+        shorterCodes = set()
+        for code in pCode[1]:
+            shorterCodes.add(code)    
+            if code.count(':') > 1:
+                shorterCode = code.rsplit(':',1)[0]
+                shorterCodes.add(shorterCode)
+
+        sortedShorterCodes = sorted(shorterCodes)
+        file.write(gCode + '\t' + '\t'.join(sortedShorterCodes) + '\n')
