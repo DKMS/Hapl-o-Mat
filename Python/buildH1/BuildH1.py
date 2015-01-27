5e605821705494cdd9d943b8b73fd57daaebd12e
Christian Schäfer <KrissSchaefer@users.noreply.github.com>
2015-01-27 15:06:51 +0100

    Write out G-groups with only one element

--- Python/buildH1/BuildH1.py
+++ Python/buildH1/BuildH1.py
@@ -3,17 +3,21 @@
 #Builds list of G-groups from hla_nom_g.txt and saves it to H1.txt
 
 with open('H1.txt', 'w') as outFile:
-    with open('hla_nom_g.txt') as file:
-        for line in file:
-            if not line.startswith('#'):
-                line = line.rstrip('\r\n')
-                splittedLine = line.split(';')
-                codeG = splittedLine[2]
-                if codeG.endswith('G'):
-                    locus = splittedLine[0]
-                    pCode = locus + codeG
-                    codes = splittedLine[1].split('/')
-                    codesWithLocus = []
-                    for code in codes:
-                        codesWithLocus.append(locus + code)
-                    outFile.write(pCode + '\t' + '\t'.join(codesWithLocus) + '\n')
+    with open('OneElementG.txt', 'w') as oneElementGFile:
+        with open('hla_nom_g.txt') as file:
+            for line in file:
+                if not line.startswith('#'):
+                    line = line.rstrip('\r\n')
+                    splittedLine = line.split(';')
+                    codeG = splittedLine[2]
+                    if codeG.endswith('G'):
+                        locus = splittedLine[0]
+                        pCode = locus + codeG
+                        codes = splittedLine[1].split('/')
+                        codesWithLocus = []
+                        for code in codes:
+                            codesWithLocus.append(locus + code)
+                        outFile.write(pCode + '\t' + '\t'.join(codesWithLocus) + '\n')
+                        if len(codes) == 1:
+                            oneElementGFile.write(pCode + '\t' + '\t'.join(codesWithLocus) + '\n')
+                        
