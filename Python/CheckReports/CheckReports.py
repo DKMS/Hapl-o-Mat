ea3eec90db0ee484fd8abaaa84e4d4c14e072791
Christian Schäfer <KrissSchaefer@users.noreply.github.com>
2015-01-28 09:48:09 +0100

    Finshied checker

--- Python/CheckReports/CheckReports.py
+++ Python/CheckReports/CheckReports.py
@@ -4,12 +4,11 @@ def checkLoci(loci):
     for locus in loci:
         if loci.count(locus) != 2:
             print 'Locus ' + locus + ' does nor occur two times.'
-            reportOkay = False
-            return 
+            return False
         if not locus.endswith('*'):
             print 'Locus ' + locus + ' misses *.'
-            reportOkay = False
-            return 
+            return False
+    return True
 
 def checkLastLetter(code, endLetters):
     lastLetter = code[-1]
@@ -17,25 +16,28 @@ def checkLastLetter(code, endLetters):
     if lastLetter.isalpha() and not preLastLetter.isalpha():
         if not lastLetter in endLetters:
             print 'Code ' + code + ' in report ' + id + ' ends with not allowed letter.'
-            reportOkay = False
+            return False
+    return True
 
 def checkForLetters(codeWithoutLetterAtTheEnd):
     splittedCodes = codeWithoutLetterAtTheEnd.split(':')
     for splittedCode in splittedCodes:
         if not splittedCode.isdigit():
             print 'Code ' + code + ' in report ' + id + ' contains not valid sign.'
-            reportOkay = False
-
+            return False
+    return True
    
     
+fileName = raw_input('Enter file name:\n')
 
 print 'Check syntax of reports:'
 
 allIds = set()
 syntacticallyCleanedReports = [] 
 firstLine = ''
+loci = []
 
-with open('reports.txt') as file:
+with open(fileName) as file:
     firstLine = file.readline()
     loci = firstLine.split()
     loci.pop(0)
@@ -46,7 +48,6 @@ with open('reports.txt') as file:
         reportOkay = True
 
         codesAtLoci = line.split()
-
         #check if id already in
         id = codesAtLoci[0]
         if id in allIds:
@@ -80,40 +81,44 @@ with open('reports.txt') as file:
             #check a 4d report
             if code.count(':') == 1:
                 endLetters = ('N', 'L', 'S', 'Q', 'g')
-                checkLastLetter(code, endLetters)
+                reportOkay = checkLastLetter(code, endLetters)
 
                 codeFirstDigit = code.split(':')[0]
-                checkForLetters(codeFirstDigit)
+                reportOkay = checkForLetters(codeFirstDigit)
 
                 codeSecondDigit = code.split(':')[1]
-                if not codeSecondDigit.isdigit():
-                    if not codeSecondDigit.isalpha():
+                if code[-1].isalpha() and not code[-2].isalpha():
+                    codeWithoutLetterAtTheEnd = codeSecondDigit[:-1]
+                else:
+                    codeWithoutLetterAtTheEnd = codeSecondDigit
+                if not codeWithoutLetterAtTheEnd.isdigit():
+                    if not codeWithoutLetterAtTheEnd.isalpha():
                         print 'Code ' + code + ' in report ' + id + ' contains not valid sign.'                        
                         reportOkay = False
 
             #check a 6d report
             elif code.count(':') == 2:
                 endLetters = ('N', 'L', 'S', 'Q', 'G')
-                checkLastLetter(code, endLetters)
+                reportOkay = checkLastLetter(code, endLetters)
                 if code[-1].isalpha() and not code[-2].isalpha():
                     codeWithoutLetterAtTheEnd = code[:-1]
                 else:
                     codeWithoutLetterAtTheEnd = code
 
-                checkForLetters(codeWithoutLetterAtTheEnd)
+                reportOkay = checkForLetters(codeWithoutLetterAtTheEnd)
 
             #check a 8d report
             elif code.count(':') == 3:
                 endLetters = ('N', 'L', 'S', 'Q')
-                checkLastLetter(code, endLetters)
+                reportOkay = checkLastLetter(code, endLetters)
 
-                checkLastLetter(code, endLetters)
+                reportOkay = checkLastLetter(code, endLetters)
                 if code[-1].isalpha() and not code[-2].isalpha():
                     codeWithoutLetterAtTheEnd = code[:-1]
                 else:
                     codeWithoutLetterAtTheEnd = code
 
-                checkForLetters(codeWithoutLetterAtTheEnd)
+                reportOkay = checkForLetters(codeWithoutLetterAtTheEnd)
 
         if reportOkay:
             syntacticallyCleanedReports.append(line)
@@ -128,7 +133,7 @@ with open('data/code2dna.txt') as file:
         key = splittedLine[0]
         codes = splittedLine[1]
         splittedCodes = codes.split('/')
-        code2dna[key] = codes
+        code2dna[key] = splittedCodes
     
 expandedAlleles = defaultdict(list)
 with open('data/allAllelesExpanded.txt') as file:
@@ -160,10 +165,59 @@ with open('data/H1g.txt') as file:
         splittedCodes = codes.split()
         H1g[key] = codes
 
-completelyCleanedReport = []
+completelyCleanedReports = []
 for report in syntacticallyCleanedReports:
+    reportOkay = True
     codes = report.split()
     codes.pop(0)
+    counter = 0
     for code in codes:
-        print code
+        #nmdp
+        if code.count(':') == 1 and code.split(':')[1].isalpha():
+            if code.split(':')[1] == 'XXX':
+                print 'Code ' + code + ' is XXX'
+                reportOkay = False
+            elif code.split(':')[1] == 'XX':
+                print 'Code ' + code + ' is XX'
+                reportOkay = False
+            elif not code.split(':')[1] in code2dna:
+                print 'Code ' + code + ' is not in code2dna.txt.'
+                reportOkay = False
+            else:
+                for combination in code2dna[code.split(':')[1]]:
+                    if ':' in combination:
+                        resolvedCode = loci[counter] + combination
+                    else:
+                        resolvedCode = loci[counter] + code.split(':')[0] + ':' + combination
+                    if not resolvedCode in expandedAlleles:
+                        print 'Code ' + resolvedCode + ' from NMPD-code ' + code + ' is not in allAllelesExpanded.txt.'
+                        reportOkay = False
+        #G
+        elif code.endswith('G'):
+            codeWithLocus = loci[counter] + code
+            if not codeWithLocus in H1:
+                print 'Code ' + codeWithLocus + ' is not in H1.txt.'
+                reportOkay = False
+        #g
+        elif code.endswith('g'):
+            codeWithLocus = loci[counter] + code
+            if not codeWithLocus in H1g:
+                print 'Code ' + codeWithLocus + ' is not in H1g.txt.'
+                reportOkay = False
+        #allAllelesExpanded
+        else:
+            codeWithLocus = loci[counter] + code
+            if not codeWithLocus in expandedAlleles:
+                print 'Code ' + codeWithLocus + ' is not in allAllelesExpanded.txt.'
+                reportOkay = False
+    
+        counter += 1
+    if reportOkay:
+        completelyCleanedReports.append(report)
 
+        
+cleanedFileName = 'cleaned' + fileName
+with open(cleanedFileName, 'w') as file:
+    file.write(firstLine)
+    for report in completelyCleanedReports:
+        file.write(report)
