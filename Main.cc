599d2934a3d0fec7c3bdaf972fd32ce1831bef7e
Christian Schäfer <KrissSchaefer@users.noreply.github.com>
2015-03-27 16:54:07 +0100

    Renaming

--- Main.cc
+++ Main.cc
@@ -30,24 +30,24 @@ int main(int argc, char *argv[]){
   double timeTakenForEMAlgorithm = 0.;
   double timeTakenForVariance = 0.;
   std::unique_ptr<Parameters> pParameters;
-  std::unique_ptr<InputFile> pData;
+  std::unique_ptr<InputFile> pInputFile;
   if(inputFileFormat == "DKMS"){
     std::unique_ptr<ParametersDKMS> pParametersTmp(new ParametersDKMS());
-    std::unique_ptr<InputFile> pDataProcessingTmp(new DKMS(*pParametersTmp));
+    std::unique_ptr<InputFile> pInputFileTmp(new DKMS(*pParametersTmp));
     pParameters = std::move(pParametersTmp);
-    pData = std::move(pDataProcessingTmp);
+    pInputFile = std::move(pInputFileTmp);
   }
   else if(inputFileFormat == "GL"){
     std::unique_ptr<ParametersGL>pParametersTmp(new ParametersGL());
-    std::unique_ptr<InputFile> pDataProcessingTmp(new GL(*pParametersTmp));
+    std::unique_ptr<InputFile> pInputFileTmp(new GL(*pParametersTmp));
     pParameters = std::move(pParametersTmp);
-    pData = std::move(pDataProcessingTmp);
+    pInputFile = std::move(pInputFileTmp);
   }
   else if(inputFileFormat == "READ"){
     std::unique_ptr<ParametersReadin>pParametersTmp(new ParametersReadin());
-    std::unique_ptr<InputFile> pDataProcessingTmp(new InputFileToRead(*pParametersTmp));
+    std::unique_ptr<InputFile> pInputFileTmp(new InputFileToRead(*pParametersTmp));
     pParameters = std::move(pParametersTmp);
-    pData = std::move(pDataProcessingTmp);
+    pInputFile = std::move(pInputFileTmp);
   }
   else{
     std::cerr << "Specify one of the input file formats (DKMS, GL, READ)" << std::endl;
@@ -56,36 +56,36 @@ int main(int argc, char *argv[]){
 
   std::cout << "#########Data-preprocessing" << std::endl;
   startTime = getTime();
-  PhenotypeList pList;
-  HaplotypeList hList(*pParameters);
-  pData->dataProcessing(pList, hList);
-  pData->printStatistics();
-  std::cout << "\t Memory requirement haplotypes: " << hList.computeSizeInBytes() << " bytes" << std::endl;
-  std::cout << "\t Memory requirement phenoypes: " << pList.computeSizeInBytes() << " bytes" << std::endl;
+  PhenotypeList phenotypes;
+  HaplotypeList haplotypes(*pParameters);
+  pInputFile->dataProcessing(phenotypes, haplotypes);
+  pInputFile->printStatistics();
+  std::cout << "\t Memory requirement haplotypes: " << haplotypes.computeSizeInBytes() << " bytes" << std::endl;
+  std::cout << "\t Memory requirement phenoypes: " << phenotypes.computeSizeInBytes() << " bytes" << std::endl;
   endTime = getTime();
   timeTakenForDataPreProcessing = getTimeDifference(startTime, endTime);
 
   std::cout << "#########EM-algorithm" << std::endl;
   startTime = getTime();
-  hList.initialiseFrequencies(pList);
-  hList.EMAlgorithm(pList);
+  haplotypes.initialiseFrequencies(phenotypes);
+  haplotypes.EMAlgorithm(phenotypes);
   endTime = getTime();
   timeTakenForEMAlgorithm = getTimeDifference(startTime, endTime);
 
   startTime = getTime();
   if(pParameters->getDoVariance()){
     std::cout << "#########Variance" << std::endl;
-    double memory = hList.getSize() * hList.getSize() * 8. / 1024. / 1024.;
+    double memory = haplotypes.getSize() * haplotypes.getSize() * 8. / 1024. / 1024.;
     if(memory >= MAX_MEMORY){
       std::cout << "\t Size of information matrix exceeds memory." << std::endl;
     }
     else{
-      pList.expectationAndRemoveStep(hList);
-      fisherInformation(hList, pList);
+      phenotypes.expectationAndRemoveStep(haplotypes);
+      fisherInformation(haplotypes, phenotypes);
     }
   }
   else{
-    hList.writeFrequenciesToFile();
+    haplotypes.writeFrequenciesToFile();
   }
   endTime = getTime();
   timeTakenForVariance = getTimeDifference(startTime, endTime);
