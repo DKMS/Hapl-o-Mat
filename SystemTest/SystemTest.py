a6888154cceee738f38bed87173316090c41f4c7
Christian Schäfer <KrissSchaefer@users.noreply.github.com>
2015-04-08 09:56:27 +0200

    script to run system test

---
+++ SystemTest/SystemTest.py
@@ -0,0 +1,75 @@
+import os
+import shutil
+import copy
+from subprocess import Popen
+
+def copyFolder(path):
+
+     pathNewFolder = path + 'New'
+     if os.path.exists(pathNewFolder):
+          print('Folder ' + pathNewFolder + ' already exists.')
+     else:
+          shutil.copytree(path, pathNewFolder)
+          os.remove(pathNewFolder + '/Log.dat')
+          os.remove(pathNewFolder + '/Err.dat')
+          os.remove(pathNewFolder + '/haplomat')
+          shutil.rmtree(pathNewFolder + '/results')
+          os.makedirs(pathNewFolder + '/results')
+          shutil.copy('../haplomat', pathNewFolder)
+
+def startJob(path):
+     proc = Popen('./runHaplomat', cwd=path, shell=True)
+     proc.wait()
+
+def readResults(path):
+     haploAndFreqs = dict()
+     with open(path + '/results/estimatedHaplotypeFrequencies.dat') as file:
+          for line in file:
+               line = line.rstrip('\r\n')
+               split = line.split()
+               haplo = split[0]
+               freq = float(split[1])
+               haploAndFreqs[haplo] = freq
+     return haploAndFreqs
+
+def compareResults(path):
+     original = readResults(path)
+     test = readResults(path + 'New')
+
+     missing = 0
+     tooMany = 0
+     different = 0
+     keys = original.keys() | test.keys()
+     for key in keys:
+          if not key in original:
+               tooMany += 1
+          if not key in test:
+               missing += 1
+          if key in original and key in test:
+               if original[key] != test[key]:
+                    different += 1
+     return missing, tooMany, different
+               
+def clean(path):
+     shutil.rmtree(path)
+
+folders = ('test', 'DKMS_g')
+testsPassed = 0
+
+for folder in folders:          
+     print('#########' + folder)
+     copyFolder(folder)
+     startJob(folder + 'New')
+     missing, tooMany, different = compareResults(folder)
+     if missing == 0 and tooMany == 0 and different == 0:
+          testsPassed += 1
+          print('Test suceeded.')
+          clean(folder + 'New')
+     else:
+          print('Test failed')
+          print('\t Missing haplotypes: ' + str(missing))
+          print('\t Additional haplotypes: ' + str(tooMany))
+          print('\t Haplotypes with different frequencies: ' + str(different))
+          print('\n')
+
+print('\n' + str(testsPassed) + '/' + str(len(folders)) + ' tests passed.' )
