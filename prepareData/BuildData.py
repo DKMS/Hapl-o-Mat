05f988a54567544cac0fc59b2fec13d734a7ca5d
Christian Schäfer <KrissSchaefer@users.noreply.github.com>
2016-06-01 12:27:13 +0200

    Automatic data processing

---
+++ prepareData/BuildData.py
@@ -0,0 +1,32 @@
+import DownloadData
+import TransferAlphaToMultipleAlleleCodes
+import BuildAllAllelesFrom_hla_nom_g
+import BuildAllAllelesExpanded
+import BuildP
+import BuildLargeG
+import BuildSmallg
+import BuildAmbiguityFromXML
+import AddGToAmbiguity
+import AddAllelesMissingIngCode
+
+def buildData():
+
+    DownloadData.downloadAndExtractData()
+
+    BuildAllAllelesFrom_hla_nom_g.buildAllAllelesFromHlaNomg()
+    BuildAllAllelesExpanded.buildAllAllelesExpanded()
+
+    BuildP.buildP()
+    BuildLargeG.buildLargeG()
+    BuildSmallg.buildSmallg()
+    AddAllelesMissingIngCode.addAllelesMissingIngCodes()
+
+    TransferAlphaToMultipleAlleleCodes.transferAlphaToMultipleAlleleCodes()
+
+    BuildAmbiguityFromXML.buildAmbiguityFromXML()
+    AddGToAmbiguity.addGToAmbiguity()
+
+
+if __name__ == "__main__":
+    
+    buildData()
