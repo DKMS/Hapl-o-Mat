a756e3782aac9f12959d5871641886251e7af2c4
Christian Schäfer <KrissSchaefer@users.noreply.github.com>
2016-06-01 11:21:39 +0200

    Added if main, print status

--- prepareData/BuildAllAllelesFrom_hla_nom_g.py
+++ prepareData/BuildAllAllelesFrom_hla_nom_g.py
@@ -32,21 +32,29 @@
 #Build list of all alleles from hla_nom_g.txt and save to allAllele.txt. 
 #Get file hla_nom_g.txt from http://hla.alleles.org/wmda/hla_nom_g.txt
 
-alleles = []
-with open('hla_nom_g.txt') as file:
-    for line in file:
-        if not line.startswith('#'):
-            line = line.rstrip('\r\n')
-            splittedLine = line.split(';')
-            locus = splittedLine[0]
-            allelesFromFile = splittedLine[1].split('/')
-            codeG = splittedLine[2]
+def buildAllAllelesFromHlaNomg():
 
-            for alleleFromFile in allelesFromFile:
-                allele = locus + alleleFromFile
-                alleles.append(allele)
+    print('Build allAlleles.txt from hla_nom_g.txt')
+    alleles = []
+    with open('hla_nom_g.txt') as file:
+        for line in file:
+            if not line.startswith('#'):
+                line = line.rstrip('\r\n')
+                splittedLine = line.split(';')
+                locus = splittedLine[0]
+                allelesFromFile = splittedLine[1].split('/')
+                codeG = splittedLine[2]
 
-with open('allAlleles.txt', 'w') as out:
-    for allele in alleles:
-        out.write(allele + '\n')
+                for alleleFromFile in allelesFromFile:
+                    allele = locus + alleleFromFile
+                    alleles.append(allele)
 
+    with open('allAlleles.txt', 'w') as out:
+        for allele in alleles:
+            out.write(allele + '\n')
+
+
+
+if __name__ == "__main__":
+
+    buildAllAllelesFromHlaNomg()
