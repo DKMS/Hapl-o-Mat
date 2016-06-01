4e80280930a7ae2bddfc5e5f38f855e42bb95b39
Christian Schäfer <KrissSchaefer@users.noreply.github.com>
2016-06-01 12:50:01 +0200

    Added moving of files and cleaning

--- prepareData/BuildData.py
+++ prepareData/BuildData.py
@@ -1,3 +1,5 @@
+import os
+import shutil
 import DownloadData
 import TransferAlphaToMultipleAlleleCodes
 import BuildAllAllelesFrom_hla_nom_g
@@ -25,8 +27,35 @@ def buildData():
 
     BuildAmbiguityFromXML.buildAmbiguityFromXML()
 
+def moveData():
 
+    print('Move produced files to ../data')
+
+    if not os.path.exists('../data'):
+        os.makedirs('../data')
+
+    shutil.move('LargeG.txt', '../data/LargeG.txt')
+    shutil.move('P.txt', '../data/P.txt')
+    shutil.move('Smallg.txt', '../data/Smallg.txt')
+    shutil.move('Ambiguity.txt', '../data/Ambiguity.txt')
+    shutil.move('MultipleAlleleCodes.txt', '../data/MultipleAlleleCodes.txt')
+    shutil.move('AllAllelesExpanded.txt', '../data/AllAllelesExpanded.txt')
+
+def clean():
+
+    print('Clean')
+
+    os.remove('alpha.v3.txt')
+    os.remove('hla_ambigs.xml')
+    os.remove('hla_nom_g.txt')
+    os.remove('hla_nom_p.txt')
+    os.remove('allAlleles.txt')
+    os.remove('OneElementG.txt')
+    shutil.rmtree('__pycache__')
+            
 
 if __name__ == "__main__":
     
     buildData()
+    moveData()
+    clean()
